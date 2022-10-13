70e2d3fce6500b4e3fd29d2880163d845ff58712
王亚非 <yh786128909@icloud.com>
2022-10-13 17:16:06 +0800

    第一次提交

---
+++ SocketExample/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { SocketService } from './socket.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule
+  ],
+  providers: [SocketService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module real-time-chat-app
+
+go 1.19
+
+require (
+	github.com/gorilla/websocket v1.5.0
+	github.com/satori/go.uuid v1.2.0
+)
